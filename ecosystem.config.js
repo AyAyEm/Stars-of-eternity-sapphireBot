c3e586ec7ee0fd36493088e91cddb2b3f9c7cff4
WarNote <andre_20113meira@hotmail.com>
2020-12-08 06:10:09 -0300

    Feat: pm2 cron restart every 24h

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
     name: 'stars-of-eternity-bot',
     watch: ['./dist'],
     watch_delay: 1000,
+    cron_restart: '0 0 0 * * ?',
     env: {
       NODE_ENV: 'production',
     },
