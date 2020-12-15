08a5a48861d8adb4a79f23ff514bdfc0ac39023f
WarNote <andre_20113meira@hotmail.com>
2020-12-15 06:58:29 -0300

    Refactor: properly using path.join now

--- src/registerPaths.ts
+++ src/registerPaths.ts
@@ -5,7 +5,7 @@ addAliases({
   '@root': __dirname,
   '@lib': path.join(__dirname, 'lib'),
   '@config': path.join(__dirname, 'config'),
-  '@utils': path.join(__dirname, 'lib/utils'),
-  '@embeds': path.join(__dirname, '/lib/embeds'),
-  '@providers': path.join(__dirname, '/lib/providers'),
+  '@utils': path.join(__dirname, 'lib', 'utils'),
+  '@embeds': path.join(__dirname, 'lib', 'embeds'),
+  '@providers': path.join(__dirname, 'lib', 'providers'),
 });
