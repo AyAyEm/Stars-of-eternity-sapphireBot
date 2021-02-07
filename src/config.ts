20a499707372444477539db517bcf7eea2b5f787
WarNote <andre_20113meira@hotmail.com>
2021-02-07 10:21:39 -0300

    Fix: ignoring config file

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+
+export const timezone = 'America/Sao_Paulo';
+
+const env = dotenv.config().parsed;
+export const config = {
+  ownersIds: ['163751711532515329'],
+  token: env.DISCORD_TOKEN || '',
+  mongoConnectionString: env.MONGO_CONNECTION_STRING || '',
+};
