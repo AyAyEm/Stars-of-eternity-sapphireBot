d03cfbc086d0ea9fa20944f47afd9c17a7e884cb
AyAyEm <andre20114meira@gmail.com>
2021-07-18 21:28:55 -0300

    Refactor(RegisterServers): removed unecessary task

--- src/tasks/RegisterServers.ts
+++
@@ -1,7 +0,0 @@
-import { Task, TaskOptions } from '@lib/structures';
-import { ApplyOptions } from '@sapphire/decorators';
-
-@ApplyOptions<TaskOptions>({ time: 10000, once: true, enabled: false })
-export default class extends Task {
-  public async run() { }
-}
