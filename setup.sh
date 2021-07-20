f794e1decf56846fcb921cf6fc6cc206ca1484b1
AyAyEm <andre20114meira@gmail.com>
2021-07-20 04:49:22 -0300

    refactor(setup): typeorm env variables

--- setup.sh
+++ setup.sh
@@ -1,11 +1,24 @@
 echo "Insert discord token"
 read DISCORD_TOKEN
 echo
-echo "Insert mongo db connection string"
-read MONGO_CONNECTION_STRING
+echo "Insert database host"
+read DB_HOST
+echo
+echo "Insert database username"
+read DB_USERNAME
+echo
+echo "Insert database password"
+read DB_PASSWORD
+echo
+echo "Insert database port"
+read DB_PORT
 
 echo "DISCORD_TOKEN=$DISCORD_TOKEN" >> .env
-echo "MONGO_CONNECTION_STRING=$MONGO_CONNECTION_STRING" >> .env
+echo "DB_HOST=$DB_HOST" >> .env
+echo "DB_USERNAME=$DB_USERNAME" >> .env
+echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+echo "DB_PORT=$DB_PORT" >> .env
+echo "NODE_ENV=production" >> .env
 
 npm install -g pm2
 npm install --production
