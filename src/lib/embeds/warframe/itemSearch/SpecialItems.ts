84f824a5789dc10c35b12c41b18dddcb670ec9c4
AyAyEm <andre20114meira@gmail.com>
2021-07-22 14:17:18 -0300

    Refator(WFSearch): now using PagedEmbed... ...and cleaned the code of both embeds and the command itself.

--- src/lib/embeds/warframe/itemSearch/SpecialItems.ts
+++ src/lib/embeds/warframe/itemSearch/SpecialItems.ts
@@ -1,16 +1,18 @@
-import type { MessageEmbed } from 'discord.js';
+import { translationBy } from '#utils';
 
-const loginWeapon = 'Arma de login\nAdquirida como opção de recompensa diária\nDe 200 em 200'
-  + ' dias a partir do 100\n**Exemplo**:\n*[100, 300, 500, 700, 900...]*';
-const sigmaWeapon = 'Arma de login\nAdquirida como opção de recompensa diária\nDe 200 em 200'
-  + ' dias a partir do 300\n**Exemplo**:\n*[300, 500, 700, 900...]*';
+import type { EternityMessageEmbed } from '#lib/extensions';
+
+const specialItemsT = translationBy('embeds/itemSearch:specialItems:');
+const embedFieldsT = translationBy('embeds/itemSearch:fields:');
+
+const loginWeapon = specialItemsT('loginWeapon', { startDay: '100', example: '100, 300, 500, 700, 900' });
+const sigmaWeapon = specialItemsT('loginWeapon', { startDay: '300', example: '300, 500, 700, 900' });
 
 function acquisitionField(value: string, inline = false) {
-  return (embed: MessageEmbed) => embed.addField('Aquisição', value, inline);
+  return (embed: EternityMessageEmbed) => embed.addField(embedFieldsT('acquisition'), value, inline);
 }
 
-export default new Map([
-  ['Athodai', acquisitionField('TennoCon 2020\nAssistindo a live por 30 minutos na Twitch')],
+export const specialItems = new Map([
   ['Azima', acquisitionField(loginWeapon)],
   ['Zenistar', acquisitionField(loginWeapon)],
   ['Zenith', acquisitionField(loginWeapon)],
---
+++ src/lib/embeds/warframe/itemSearch/Warframe.ts
@@ -0,0 +1,124 @@
+import _ from 'lodash';
+
+import { EternityMessageEmbed } from '#lib/extensions';
+import { InitPagedEmbed, Page } from '#decorators';
+import { ItemUtilities, masteryRankImgs } from '#utils';
+import { BaseItemPagedEmbed } from './BaseItem';
+
+@InitPagedEmbed()
+export class WarframePagedEmbed extends BaseItemPagedEmbed {
+  public bpSource = ItemUtilities.blueprintSource(this.item);
+
+  public baseEmbed() {
+    const { name, imageName, masteryReq, category } = this.item;
+
+    return new EternityMessageEmbed()
+      .setTitle(`${name}`)
+      .setThumbnail(`https://cdn.warframestat.us/img/${imageName}`)
+      .addField(this.t('fields:category'), category, false)
+      .setFooter(`${this.t('footer:mastery')} ${masteryReq}`, masteryRankImgs[masteryReq || 0]);
+  }
+
+  @Page({ emoji: '📋' })
+  public mainInfo() {
+    const { components = [] } = this.item;
+    const embed = this.baseEmbed();
+
+    embed.addField(
+      this.t('fields:status'),
+      this.t('warframe:fields:status', this.item),
+      false,
+    );
+
+    const prime = ItemUtilities.isPrime(this.item);
+
+    const [resources, componentItems] = ItemUtilities.partitionComponents(components, prime);
+
+    const { bpSource } = this;
+    if ('location' in bpSource && 'id' in bpSource && !prime) {
+      const blueprintString = bpSource.id === 1
+        ? `${bpSource.location} Lab: ${bpSource.lab}`
+        : `${bpSource.location}`;
+
+      embed.addField(this.t('fields:blueprint'), blueprintString, false);
+    }
+
+    if (componentItems.length > 0) {
+      const componentsString = componentItems
+        .map(({ name, itemCount }) => `${name} **${itemCount}**`)
+        .join('\n');
+
+      embed.addField(this.t('fields:components'), componentsString, false);
+    }
+
+    if (resources.length > 0) {
+      const resourcesNames = resources.map(({ name: resourceName, itemCount }) => (
+        `${resourceName} **${itemCount}**`));
+
+      embed.addField(this.t('fields:resources'), resourcesNames.join('\n'), false);
+    }
+
+    return embed;
+  }
+
+  @Page({ emoji: '♻' })
+  public componentsPage() {
+    if (!this.item.components || this.item.components.length === 0) {
+      return null;
+    }
+
+    const { components, category } = this.item;
+    const embed = this.baseEmbed();
+
+    if (ItemUtilities.isPrime(this.item)) {
+      const componentsFields = ItemUtilities.filterForPrimeComponents(components)
+        .sort(({ name }) => (name === 'Blueprint' ? -1 : 1))
+        .map(({ name, drops }) => {
+          const bestDropsString = ItemUtilities.dropsString(ItemUtilities.bestDrops(drops));
+
+          return { name, value: bestDropsString, inline: false };
+        });
+
+      embed.addFields(...componentsFields);
+    } else {
+      const [resources, componentItems] = ItemUtilities.partitionComponents(components, true);
+
+      if (category === 'Warframes') {
+        const componentsFields = componentItems
+          .filter(({ drops }) => drops && drops.length > 0)
+          .map(({ drops, name }) => {
+            const nameAndChance = _.uniqBy(drops, 'location')
+              .map((drop) => ItemUtilities.dropToNameAndChance(drop))
+              .sort(({ chance: a }, { chance: b }) => {
+                if (a === b) return 0;
+                return a < b ? 1 : -1;
+              })
+              .slice(0, 3);
+
+            const dataString = nameAndChance
+              .map(({ name: enemyName, chance }) => `${enemyName} **${Math.round(chance * 100) / 100}%**`)
+              .join('\n');
+
+            return { name, value: dataString, inline: false };
+          });
+
+        embed.addFields(componentsFields);
+      } else if (category === 'Archwing') {
+        const componentsFields = componentItems
+          .map(({ name }) => ({ name, value: 'Tenno lab', inline: false }));
+
+        embed.addFields(componentsFields);
+      }
+
+      if (resources.length > 0) {
+        const resourcesString = resources
+          .map(({ name, itemCount }) => `${name} **${itemCount}**`)
+          .join('\n');
+
+        embed.addField(this.t('fields:resources'), resourcesString, false);
+      }
+    }
+
+    return embed;
+  }
+}
