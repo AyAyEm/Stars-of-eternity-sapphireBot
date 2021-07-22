b885ac9ac105a073fa504686efd202b1dae58cac
AyAyEm <andre20114meira@gmail.com>
2021-07-22 15:35:36 -0300

    Refactor(connection): disabled rejectUnauthorized

--- src/lib/typeorm/connection.ts
+++ src/lib/typeorm/connection.ts
@@ -15,7 +15,9 @@ export const config: ConnectionOptions = {
   port: +process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: 'starsOfEternity',
+  ssl: {
+    rejectUnauthorized: false,
+  },
   synchronize: false,
   logging: process.env.NODE_ENV === 'development' ? 'all' : false,
   namingStrategy: new SnakeNamingStrategy(),
