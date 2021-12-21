5ae48f49806dbb01d2706551814e33dda9975c9c
AyAyEm <42449362+AyAyEm@users.noreply.github.com>
2021-12-21 10:05:10 -0300

    Fix: added user and password to mongo connection

--- src/lib/mongodb/connection.ts
+++ src/lib/mongodb/connection.ts
@@ -4,4 +4,7 @@ const host = process.env.MONGO_HOST ?? 'localhost';
 const port = process.env.MONGO_PORT ?? '27017';
 const db = process.env.MONGO_DB_NAME ?? 'starsOfEternity';
 
-export const mongoConnect = () =>  mongoose.connect(`mongodb://${host}:${port}/${db}`);
+export const mongoConnect = () => mongoose.connect(`mongodb://${host}:${port}/${db}`, { 
+  user: process.env.MONGO_USERNAME, 
+  pass: process.env.MONGO_PASSWORD, 
+});
