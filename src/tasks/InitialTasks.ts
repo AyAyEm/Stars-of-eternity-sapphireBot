40f252241cd484c969939d69b58fe7257958b0b8
AyAyEm <42449362+AyAyEm@users.noreply.github.com>
2021-12-22 14:54:46 -0300

    Fix: now completely removed discordjs/voice

--- src/tasks/InitialTasks.ts
+++ src/tasks/InitialTasks.ts
@@ -1,13 +1,10 @@
-import { getVoiceConnection } from '@discordjs/voice';
 import { ApplyOptions } from '@sapphire/decorators';
 
 import { Task, TaskOptions } from '#lib/structures';
 
 @ApplyOptions<TaskOptions>({ time: 10000, once: true })
 export default class extends Task {
-  public async run() {
-    this.container.client.guilds.cache.each((guild) => getVoiceConnection(guild.id)?.destroy());
-  
+  public async run() {  
     this.container.client.emit('initialTasksCompleted');
   }
 }
