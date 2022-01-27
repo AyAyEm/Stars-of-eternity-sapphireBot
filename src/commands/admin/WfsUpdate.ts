f5245f8db996bdeab0cecce5005cd1d0dbbcb15f
AyAyEm <42449362+AyAyEm@users.noreply.github.com>
2022-01-27 11:19:03 -0300

    Refactor: wfsUpdate to sapphire v2

--- src/commands/admin/WfsUpdate.ts
+++ src/commands/admin/WfsUpdate.ts
@@ -1,16 +1,17 @@
-import { EternityCommand } from '@lib';
+import { Message } from 'discord.js';
 import { ApplyOptions } from '@sapphire/decorators';
+import { replyLocalized } from '@sapphire/plugin-i18next';
 
-import type { EternityCommandOptions, EternityMessage } from '@lib';
+import { EternityCommand, EternityCommandOptions } from '#lib';
 
 @ApplyOptions<EternityCommandOptions>({
   preconditions: ['OwnerOnly'],
   aliases: ['wfs-update', 'wfsu', 'wfs-u', 'warframeItems-update'],
 })
 export default class extends EternityCommand {
-  public async run(msg: EternityMessage) {
-    msg.replyTranslated('commands/WfsUpdate:updating');
-    await this.client.warframe.items.create();
-    msg.replyTranslated('commands/WfsUpdate:success');
+  public async messageRun(msg: Message) {
+    replyLocalized(msg, 'commands/WfsUpdate:updating');
+    await this.container.warframe.items.create();
+    replyLocalized(msg, 'commands/WfsUpdate:success');
   }
 }
